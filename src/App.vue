49adf2b30e600d90553752974e70eb09c3ea2186
Your Name <dotasyahrul@gmail.com>
2019-03-16 22:11:02 +0700

    commit in submodule

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar toggleable="lg" type="light" variant="faded">
+      <b-navbar-brand href="#"><font-awesome-icon icon="graduation-cap"></font-awesome-icon> Sekolah Pintar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav_collapse" />
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          
+
+          <b-nav-item  right>
+            <router-link to="/pelajaran"><b-button variant="outline-success">Pelajaran</b-button></router-link>
+          </b-nav-item>
+          <b-nav-item  right>
+            <router-link to="/guru"><b-button variant="outline-success">Guru</b-button></router-link>
+          </b-nav-item>
+          
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
